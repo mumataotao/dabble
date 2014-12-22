51b99c04e5a85ea5b03e8abbaeb604ea6c7e5807
yangchao <yangchao3220@qq.com>
2014-12-22 21:45:39 +0800

    代码初始化

---
+++ js/footer.js
@@ -0,0 +1,6 @@
+ /* 底部写入相关信息
+  * ================== */
+
+window.onload = function () { 
+	document.getElementById("footer").innerHTML='<div class="container"><hr />		<p>&copy; 2014  <a href="http://dabble.sinaapp.com/" target="_blank" title="剪贴板">剪贴板</a> - 传送门  <a href="http://blog.csdn.net/mumataotao" title="项目博客" target="_blank">项目博客</a></p>	</div> <!-- /container -->' 
+}
